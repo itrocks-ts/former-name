ce8a068fb771f27b27fcd59754ee398bfcd729c0
Baptiste Pillot <baptiste@pillot.fr>
2026-03-13 09:52:08 +0100

    Apply support for symbol and numeric property keys

--- src/former-name.ts
+++ src/former-name.ts
@@ -1,5 +1,6 @@
-import { KeyOf, ObjectOrType }   from '@itrocks/class-type'
-import { decorate, decoratorOf } from '@itrocks/decorator/property'
+import { ObjectOrType } from '@itrocks/class-type'
+import { decorate }     from '@itrocks/decorator/property'
+import { decoratorOf }  from '@itrocks/decorator/property'
 
 const FORMER_NAME = Symbol('formerName')
 
@@ -8,7 +9,7 @@ export function FormerName<T extends object>(...formerName: string[])
 	return decorate<T>(FORMER_NAME, formerName)
 }
 
-export function formerNameOf<T extends object>(target: ObjectOrType<T>, property: KeyOf<T>)
+export function formerNameOf<T extends object>(target: ObjectOrType<T>, property: keyof T)
 {
 	return decoratorOf<string[], T>(target, property, FORMER_NAME, [])
 }
